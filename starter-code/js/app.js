a68b0757c36b9024adff146bfce59421e726beb8
EvanBC1 <starfoxz13@gmail.com>
2019-05-16 17:15:24 -0700

    fixes

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -10,8 +10,6 @@ var Cart = function(items, quantity) {
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
 };
-addItems();
-
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
